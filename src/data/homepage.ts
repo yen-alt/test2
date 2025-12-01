5f47c3ee410b40153645126b322f76ad6aae4270
yen-alt <phuang76@asu.edu>
2025-11-30 22:47:20 -0700

    Initial commit

---
+++ src/data/homepage.ts
@@ -0,0 +1,34 @@
+export const homepage = {
+  sections: [
+    {
+      name: `Introduction`,
+      variant: `default`,
+      enabled: true
+    },
+    {
+      name: `Experience`,
+      variant: `default`,
+      enabled: true
+    },
+    {
+      name: `Publications`,
+      variant: `default`,
+      enabled: true
+    },
+    {
+      name: `Projects`,
+      variant: `default`,
+      enabled: true
+    },
+    {
+      name: `Talks`,
+      variant: `default`,
+      enabled: true
+    },
+    {
+      name: `Service`,
+      variant: `default`,
+      enabled: true
+    }
+  ]
+};
---
+++ src/data/skills.ts
@@ -0,0 +1,13 @@
+export const skills = {
+  programming: [
+    `C++`,
+    `Python`,
+    `JavaScript`
+  ],
+  technologies: [
+    `React`,
+    `TailwindCSS`,
+    `Next.js`,
+    `AWS`
+  ]
+};
